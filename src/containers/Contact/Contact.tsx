6c3127f7f919e2916a2c193a10b879c3d6c8ea20
Michelle Williams <michellewilliams@Michelles-MacBook-Pro.local>
2025-01-03 18:53:32 -0700

    add contact messaging

--- src/containers/Contact/Contact.tsx
+++ src/containers/Contact/Contact.tsx
@@ -1,9 +1,87 @@
 // BASE MODULES
+import { useState } from 'react';
+import emailjs from '@emailjs/browser';
 
 // CUSTOM MODULES
+import Bar from '../../components/Bar/Bar';
+import { isDesktop } from '../../utilities/responsiveness';
+import useWindowSize from '../../hooks/windowSize';
+import styles from './Contact.module.scss';
 
 const Contact = () => {
-  return <div>Contact</div>;
+  const windowSize = useWindowSize();
+
+  const [name, setName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [message, setMessage] = useState<string>('');
+
+  const [messageSuccessful, setMessageSuccessful] = useState<boolean>(false);
+  const [showMessageResults, setShowMessageResults] = useState<boolean>(false);
+
+  // SEND MESSAGE
+  const sendMessage = () => {
+    emailjs
+      .send(
+        import.meta.env.VITE_SERVICE_ID,
+        import.meta.env.VITE_TEMPLATE_ID,
+        { name, email, message },
+        { publicKey: import.meta.env.VITE_PUBLIC_KEY }
+      )
+      .then(
+        (result) => {
+          console.log('Message sent successfully:', result.text);
+          setShowMessageResults(true);
+          setMessageSuccessful(true);
+        },
+        (error) => {
+          console.error('Error sending message:', error);
+          setShowMessageResults(true);
+          setMessageSuccessful(false);
+        }
+      );
+  };
+
+  return (
+    <section className={styles.ContactContainer}>
+      <h2 className={styles.TitleCopy}>Get in Touch</h2>
+
+      <Bar
+        bgColor={isDesktop(windowSize) ? '#5448a1' : '#fff'}
+        width={isDesktop(windowSize) ? '45%' : '75%'}
+      />
+
+      <section className={styles.BodyCopyContainer}>
+        <p>Want to chat about future work?</p>
+
+        <p>Feel free to reach out!</p>
+      </section>
+
+      <section className={styles.ContactFormContainer}>
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Name"
+          type="text"
+        />
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          type="text"
+        />
+        <input
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="Message"
+          type="text"
+        />
+      </section>
+
+      <button type="button" onClick={sendMessage}>
+        Send
+      </button>
+    </section>
+  );
 };
 
 export default Contact;
