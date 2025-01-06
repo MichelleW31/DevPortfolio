bf0ed65204714f06aad04bb4f176cb948c8f0388
Michelle Williams <michellewilliams@Michelles-MacBook-Pro.local>
2025-01-06 13:16:17 -0700

    fix type for deployment

--- src/components/BackButtonArrow/BackButtonArrow.tsx
+++ src/components/BackButtonArrow/BackButtonArrow.tsx
@@ -1,11 +1,15 @@
 // BASE MODULES
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
 
 // CUSTOM MODULES
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleLeft } from '@fortawesome/free-solid-svg-icons';
 
 const BackButtonArrow = () => (
-  <FontAwesomeIcon style={{ color: '#5448a1' }} icon={faAngleLeft} />
+  <FontAwesomeIcon
+    style={{ color: '#5448a1' }}
+    icon={faAngleLeft as IconProp}
+  />
 );
 
 export default BackButtonArrow;
