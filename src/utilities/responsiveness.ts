c44820e081738bc62187f7eb9b66f2bc4f21b456
Michelle Williams <michellewilliams@Michelles-MacBook-Pro.local>
2025-01-06 22:22:03 -0700

    fix background and height issues

--- src/utilities/responsiveness.ts
+++ src/utilities/responsiveness.ts
@@ -1,7 +1,7 @@
 export const isMobile = (resolution: number) => {
-  return resolution >= 320 && resolution <= 1280;
+  return resolution >= 320 && resolution < 1280;
 };
 
 export const isDesktop = (resolution: number) => {
-  return resolution > 1280;
+  return resolution >= 1280;
 };
